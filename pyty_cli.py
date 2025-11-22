9c3bfc79aa31832065fc9a20ead611f6b4d453ec
wfarnsworth110 <walkerfarnsworth@gmail.com>
2025-11-22 01:43:16 -0500

    Created .gitignore to ignore virtual environment configuration.
    Started pyty_cli.py and included the imports and method definitions. Next will be implementing each method.

---
+++ pyty_cli.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+import subprocess
+import tempfile
+from pathlib import Path
+from rich import print, box
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+def detect_type_error(filepath):
+    pass
+
+def build_input_json(source_code, error_data):
+    pass
+
+def run_pyty(json_input):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
